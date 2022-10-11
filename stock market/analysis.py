5145fd2e68b05b65305f034ebb09fd64dc145fdd
akhilaakhi27 <96654491+akhilaakhi27@users.noreply.github.com>
2022-10-11 15:40:00 +0530

    Add files via upload

---
+++ stock market/analysis.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+data=pd.read_excel('pricehistory.xlsx',index_col=None,na_values=['NA'],usecols="B,E")
+df=pd.DataFrame(data)
+print(df)
+x=np.array(df['OPEN']).reshape(-1,1)
+y=np.array(df['CLOSE']).reshape(-1,1)
+df.dropna(inplace=True)
+print(x)
+print(y)
+plt.plot(x,y)
+plt.show()
