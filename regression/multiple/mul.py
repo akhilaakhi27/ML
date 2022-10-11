5e174f4255d2c4cc70006708536978a9524b3478
akhilaakhi27 <96654491+akhilaakhi27@users.noreply.github.com>
2022-10-11 15:48:54 +0530

    Add files via upload

---
+++ regression/multiple/mul.py
@@ -0,0 +1,28 @@
+import imp
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error,mean_absolute_error
+from sklearn import preprocessing
+df = pd.read_csv('real.csv')
+df.drop('No', inplace=True, axis=1)
+print(df.head())
+print(df.columns)
+sns.scatterplot(x='X4 number of convenience stores',y='Y house price of unit area', data=df)
+plt.show()
+X = df.drop('Y house price of unit area', axis=1)
+y = df['Y house price of unit area'] 
+print(X)
+print(y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=101)
+model = LinearRegression()
+model.fit(X_train, y_train)
+predictions = model.predict(X_test)
+print('mean_squared_error : ', mean_squared_error(y_test, predictions))
+print('mean_absolute_error : ', mean_absolute_error(y_test, predictions))
+  
